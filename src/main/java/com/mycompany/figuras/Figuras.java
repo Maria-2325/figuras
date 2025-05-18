dd018a20653c749986f9643ac23883e5d426b605
Licet <marialiset13@gmail.com>
2025-05-17 23:13:44 -0500

    Ignore

--- src/main/java/com/mycompany/figuras/Figuras.java
+++ src/main/java/com/mycompany/figuras/Figuras.java
@@ -6,7 +6,7 @@ public class Figuras {
         System.out.println("\n\t---------Circulo----------");
         Circulo objCirculo = new Circulo(5d);
         System.out.println("Datos: ");
-        System.out.println("El radio es: " + objCirculo.radio);
+        System.out.println("El radio es: " + objCirculo.radio + " cm" );
         System.out.println("El area del circulo es: " + objCirculo.calcularArea() + " cm^2");
         System.out.println("El perimetro del circulo es: " + objCirculo.calcularPerimetro() + " cm" );
 
@@ -14,7 +14,7 @@ public class Figuras {
         System.out.println("\n\t-----Cuadrado-----");
         Cuadrado objCuadrado = new Cuadrado(12d);
         System.out.println("Datos: ");
-        System.out.println("El lado es: " + objCuadrado.lado);
+        System.out.println("El lado es: " + objCuadrado.lado + " cm" );
         System.out.println("El area del cuadrado es: " + objCuadrado.calcularArea() + " cm^2");
         System.out.println("El perimetro del cuadrado es: " + objCuadrado.calcularPerimetro() + " cm" );
 
@@ -22,8 +22,8 @@ public class Figuras {
         System.out.println("\n\t-----Rectangulo--------");
         Rectangulo objRectangulo = new Rectangulo(4d, 5d);
         System.out.println("Datos: ");
-        System.out.println("La base es: " + objRectangulo.base);
-        System.out.println("La altura es: " + objRectangulo.altura);
+        System.out.println("La base es: " + objRectangulo.base + " cm" );
+        System.out.println("La altura es: " + objRectangulo.altura + " cm" );
         System.out.println("El area del rectangulo es: " + objRectangulo.calcularArea() + " cm^2");
         System.out.println("El perimetro del rectangulo es: " + objRectangulo.calcularPerimetro() + " cm" );
 
@@ -31,8 +31,8 @@ public class Figuras {
         System.out.println("\n\t---------Triangulo Rectangulo------");
         TrianguloRectangulo objTrianguloRectangulo = new TrianguloRectangulo(8d, 16d);
         System.out.println("Datos: ");
-        System.out.println("La base es: " + objTrianguloRectangulo.base);
-        System.out.println("La altura es: " + objTrianguloRectangulo.altura);
+        System.out.println("La base es: " + objTrianguloRectangulo.base + " cm" );
+        System.out.println("La altura es: " + objTrianguloRectangulo.altura + " cm" );
         System.out.println("El area del triangulo rectangulo es: " + objTrianguloRectangulo.calcularArea() + " cm^2" );
         System.out.println("El perimetro del triangulo rectangulo es: " + objTrianguloRectangulo.calcularPerimetro() + " cm" );
         System.out.println("La hipotenusa del triangulo rectangulo es: " + objTrianguloRectangulo.calcularHipotenusa() + " cm" );
